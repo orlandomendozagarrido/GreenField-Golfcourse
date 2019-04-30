aaca8a9474785b251f0435057c339f25a31cce6c
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2019-04-30 11:25:47 -0500

    working on react lifecycles for editor box

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -9,16 +9,37 @@ import React from 'react';
 class Editor extends React.Component {
    constructor(props){
    	super(props);
-  
+      this.state = {
+       updateEditor : "",
+        data: ""
+      }
+      this.handleUpdate = this.handleUpdate.bind(this);
    }
+
+
+  componentDidUpdate(prevProps , currProps){
+    if (prevProps.data !== this.props.data) {
+   this.updateEditor = Editor.load({
+     data: this.props.data
+   });
+ }
+
+
+  }
+  handleUpdate(e){
+
+
+  }
+
+
   render() {
     return (
 
       <div>
-      
+
        <textarea onChange={this.props.update}> </textarea>
-         
-     
+
+
      </div>
      )
        }
